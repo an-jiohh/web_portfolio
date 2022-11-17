5ccee9e0729cf93a0e23758da5cc34f1016eea15
an-jiohh <dkswlgh99@naver.com>
2022-11-17 22:05:54 +0900

    update index_archive ejs

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+"use strict";
+
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/archiving", function (req, res, next) {
+  res.render("archiving");
 });
 
 module.exports = router;
